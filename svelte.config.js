71e0a505cd5f62f2b993ddbef5d448239ab48e57
Nasfame <laciferin2024@protonmail.com>
2024-09-24 17:39:28 +0530

    compilerOptions: dev true

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+
 /** @type {import('@sveltejs/vite-plugin-svelte').SvelteConfig} */
 const config = {
   preprocess: vitePreprocess(),  // Enables preprocessing like TypeScript or SCSS
+  compilerOptions: { dev: true, },
 };
 
 export default config;

ca768a810a9dc939ee8c04aa4cafdfe432f0b845
Nasfame <laciferin2024@protonmail.com>
2024-09-24 17:39:43 +0530

    import from astro?

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),  // Enables preprocessing like TypeScript or SCSS
   compilerOptions: { dev: true, },
+  // TODO: is dev import from astro
 };
 
 export default config;
